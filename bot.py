cd18efc812b0b862530720ca2da8e8eae86a0b9d
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-14 22:48:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,16 +26,17 @@ with sync_playwright() as p:
 
     page2 = browser.newPage()
 
-    # Log in to YouTube using environment variables
-    page2.goto('https://www.youtube.com/signin', timeout = 30000)
-    page2.fill('#identifierId', os.environ['YOUTUBE_USERNAME'])
+    # Log in to YouTube
+    page2.goto('https://www.youtube.com/signin')
+    page2.fill('#identifierId', '<your-username>')
     page2.click('#identifierNext')
-    page2.waitForSelector('input[type="password"]')
-    page2.fill('input[type="password"]', os.environ['YOUTUBE_PASSWORD'])
+    page2.fill('input[type="password"]', '<your-password>')
     page2.click('#passwordNext')
 
     # Upload video to YouTube channel 
     page2.goto('https://www.youtube.com/upload') 
-    page2.waitForSelector('.upload-prompt-box input[type="file"]') 
-    fileInput = page2.$('.upload-prompt-box input[type="file"]') 
-    fileInput._upload([os.path.abspath("video.mp4")])
+    file_input = page2.querySelector('input[type="file"]') 
+    file_input.uploadFile(os.path.abspath("video.mp4")) 
+
+    # Publish video 
+    page2.click("#submit-upload-button")
